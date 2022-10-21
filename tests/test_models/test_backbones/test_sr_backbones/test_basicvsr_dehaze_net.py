3473921ed215466d6888f0a147ead22e6e226cdc
moonfall <18321299378@163.com>
2022-10-21 11:57:17 +0800

    Initial commit

---
+++ tests/test_models/test_backbones/test_sr_backbones/test_basicvsr_dehaze_net.py
@@ -0,0 +1,16 @@
+import torch
+from mmedit.models.backbones.sr_backbones.basicvsr_dehaze_net import BasicVSRDehazeNet
+
+
+if __name__ == "__main__":
+    # output has the same size as input
+    model = BasicVSRDehazeNet(
+        mid_channels=64,
+        num_blocks=30,
+        is_low_res_input=False,
+        spynet_pretrained=None).cuda()
+    input_tensor = torch.rand(1, 5, 3, 256, 256).cuda()
+    output = model(input_tensor)
+    assert output.shape == (1, 5, 3, 256, 256)
+    
+    
